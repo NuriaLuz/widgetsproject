d2dfc480feaccbcbe9e7c519c4303b967694a629
Nuria Rodriguez <nurialuzr1@gmail.com>
2020-09-27 20:11:18 -0400

    added a few semantic-ui css style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import Accordion from './components/Accordion'
+
+const items = [
+    {
+        title: 'What is React?',
+        content : 'React is a front end JavaScript framework'
+    },
+    {
+        title: 'What is CSS?',
+        content: 'Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML'
+    }
+
+]
 
 export default () => {
     return (
-    <h1>
-        Widgets Application!
-    </h1>
+    <div>
+       <Accordion items = {items} />
+    </div>
     )
 };
