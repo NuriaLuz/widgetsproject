bbb2e0bceaed6a437b896d7db65182d5a3eba040
Nuria Rodriguez <nurialuzr1@gmail.com>
2020-09-29 02:20:19 -0400

    list is rendering results

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,23 +1,41 @@
-import React, { useState , useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const Search = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([])
 
     useEffect(() => {
-    const search = async() => {
-        await axios.get('https://en.wikipedia.org/w/api.php',{
-            params:{
-                action:'query',
-                list:'search',
-                origin:'*',
-                format:'json',
-                srsearch: term
-            }
-        })
-    };
-    search()
-    },[term])
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            })
+            setResults(data.query.search)
+        };
+        if (term) {
+            search()
+        } //if the user doesn't put anything to search, then don't invoke the search function
+    }, [term])
+
+    const renderedResults = results.map((result) => {
+        return(
+            <div className="item" key={result.pageid}>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    {result.snippet}
+                </div>
+            </div>
+
+        )
+    })
 
     return (
         <div>
@@ -29,7 +47,10 @@ const Search = () => {
                         value={term}
                         onChange={event => setTerm(event.target.value)}
                     />
-                </div>
+                </div>  
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
 
